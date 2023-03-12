bb4a6773c833a39026e0c7eefbfd5540c5d8962d
Kiselev Alexandr <yell7reserve@gmail.com>
2023-03-12 19:25:36 +0300

    Auth/login

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,10 +1,42 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { AppService } from "./app.service";
+import { SequelizeModule } from "@nestjs/sequelize";
+import { UsersModule } from './users/users.module';
+import { ConfigModule } from "@nestjs/config";
+import { User } from "./users/users.model";
+import { RolesModule } from './roles/roles.module';
+import { Role } from "./roles/roles.model";
+import { UserRoles } from "./roles/user-roles.model";
+import { AuthModule } from './auth/auth.module';
+// import { PostsModule } from './posts/posts.module';
+// import { Post } from "./posts/posts.model";
+// import { FilesModule } from './files/files.module';
+// import { ServeStaticModule } from "@nestjs/serve-static";
+import * as path from 'path'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    controllers: [],
+    providers: [],
+    imports: [
+        ConfigModule.forRoot({
+            envFilePath: `.${process.env.NODE_ENV}.env`
+        }),
+        // ServeStaticModule.forRoot({
+        //   rootPath: path.resolve(__dirname, 'static')
+        // }),
+        SequelizeModule.forRoot({
+          dialect: 'postgres',
+          host: process.env.POSTGRES_HOST,
+          port: Number(process.env.POSTGRESS_PORT),
+          username: process.env.POSTGRES_USER,
+          password: process.env.POSTGRESS_PASSWORD,
+          database: process.env.POSTGRES_DB,
+          models: [User, Role, UserRoles],
+          autoLoadModels: true,
+        }),
+        UsersModule,
+        RolesModule,
+        AuthModule,
+      ],
 })
 export class AppModule {}
--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,26 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { DocumentBuilder } from "@nestjs/swagger";
+import { SwaggerModule } from "@nestjs/swagger/dist";
+// import { ValidationPipe } from "./pipes/validation.pipe";
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+async function start(){
+  const PORT = process.env.PORT || 500;
+  const app = await NestFactory.create(AppModule)
+
+  const config = new DocumentBuilder()
+    .setTitle('kada')
+    .setDescription('Документация REST API')
+    .setVersion('1.0.0')
+    .addTag('Курсовая работа')
+    .build()
+
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('/api/docs', app, document)
+
+  // app.useGlobalPipes(new ValidationPipe())
+
+  await app.listen(PORT, () => console.log(`Server started on port = ${PORT}`))
 }
-bootstrap();
+
+start()
