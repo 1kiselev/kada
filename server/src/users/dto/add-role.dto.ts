251f61ddb9f8fee0b1775085e4a7581b1fe29dec
Kiselev Alexandr <yell7reserve@gmail.com>
2023-04-03 18:38:22 +0300

    Added group format

--- server/src/users/dto/add-role.dto.ts
+++ server/src/users/dto/add-role.dto.ts
@@ -1,8 +1,11 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsNumber, IsString } from "class-validator";
 
 export class AddRoleDto{
+    @ApiProperty({example: 'ADMIN', description: 'Название роли'})
     @IsString({message: 'Должно быть строкой'})
     readonly value: string;
+    @ApiProperty({example: '3', description: 'id пользователя группы'})
     @IsNumber({}, {message: 'Должно быть числом'})
     readonly user_group_id: number;
 }
